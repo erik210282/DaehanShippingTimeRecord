7b7d1ced275987862c537afe86647fd34587e351
Erik Lopez <erik@dhsc.co.kr>
2025-06-01 10:00:03 -0700

    idx

--- src/pages/Registros.jsx
+++ src/pages/Registros.jsx
@@ -545,7 +545,19 @@ const cargarCatalogos = async () => {
       
         <Modal isOpen={modalAbierto} onRequestClose={() => setModalAbierto(false)}>
           <h3>{esNuevo ? t("add") : t("edit")}</h3>
-          <textarea value={registroActual?.idx} onChange={(e) => setRegistroActual({ ...registroActual, idx: e.target.value })} placeholder={t("idx")} rows={2} style={{ width: "100%", marginTop: 10 }} />  
+
+              <input
+                type="number"
+                placeholder={t("idx")}
+                value={p.idx}
+                onChange={(e) => {
+                  const nuevos = [...registroActual.productos];
+                  nuevos[index].idx = e.target.value;
+                  setRegistroActual({ ...registroActual, productos: nuevos });
+                }}
+                style={{ width: "400px" }}
+              />
+
           <Select options={selectActividades} value={selectActividades.find((i) => i.value === registroActual?.actividad)} onChange={(e) => setRegistroActual({ ...registroActual, actividad: e.value })} placeholder={t("select_activity")} />
 
           {registroActual?.productos?.map((p, index) => (
