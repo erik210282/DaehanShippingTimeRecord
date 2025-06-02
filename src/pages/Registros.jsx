7df6edd6cbb02664022428e5e5aac3f84d643c5f
Erik Lopez <erik@dhsc.co.kr>
2025-06-02 14:51:23 -0700

    buton

--- src/pages/Registros.jsx
+++ src/pages/Registros.jsx
@@ -463,7 +463,8 @@ const cargarCatalogos = async () => {
               ...registroActual,
               productos: [...registroActual.productos, { producto: "", cantidad: "" }],
             })}
-            style={{ marginBottom: 10 }}>
+            style={{ marginBottom: 20 }}
+          >
             ➕ {t("add_product")}
           </button>
 
@@ -633,15 +634,7 @@ const cargarCatalogos = async () => {
               )}
             </div>
           ))}
-          <button
-            onClick={() => setRegistroActual({
-              ...registroActual,
-              productos: [...registroActual.productos, { producto: "", cantidad: "" }],
-            })}
-            style={{ marginTop: "10px" }}
-          >
-            ➕ {t("add_product")}
-          </button>
+          
           <Select isMulti options={selectOperadores} value={selectOperadores.filter((i) => registroActual?.operadores?.includes(i.value))} onChange={(e) => setRegistroActual({ ...registroActual, operadores: e.map((i) => i.value) })} placeholder={t("select_operator")} />
           <textarea value={registroActual?.notas} onChange={(e) => setRegistroActual({ ...registroActual, notas: e.target.value })} placeholder={t("notes")} rows={2} style={{ width: "100%", marginTop: 10 }} />
           <input type="datetime-local" value={registroActual?.horaInicio} onChange={(e) => setRegistroActual({ ...registroActual, horaInicio: e.target.value })} />
