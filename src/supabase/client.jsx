22314f1ee4120730573809cc86b23bcae7bf8d7f
Erik Lopez <erik@dhsc.co.kr>
2025-06-10 15:13:45 -0700

    d

--- src/supabase/client.jsx
+++ src/supabase/client.jsx
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wqpceonfeyjfegawpzpv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcGNlb25mZXlqZmVnYXdwenB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NjI3MDcsImV4cCI6MjA2NTEzODcwN30.8rUaWkcJXmwKZvvw2npmYAp76rblTm0n-j2GjcevohQ';
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
