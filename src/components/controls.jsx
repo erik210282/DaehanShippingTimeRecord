dcfa9ab502b7c1319a3b98fbddecccafb095e14e
Erik Lopez <erik@dhsc.co.kr>
2025-11-12 14:07:06 -0800

    d

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -1,4 +1,5 @@
 // src/components/controls.jsx
+import React from "react";
 import Select, { components } from "react-select";
 import {
   inputStyle,
@@ -11,14 +12,16 @@ import {
   tinyRoundBtn,
 } from "./styles";
 
-export const DSInput = (props) => (
-  <input {...props} style={{ ...inputStyle, ...props.style }} />
+// --------- Inputs básicos ----------
+export const DSInput = ({ style, ...props }) => (
+  <input {...props} style={{ ...inputStyle, ...style }} />
 );
 
-export const DSDate = (props) => (
-  <input type="date" {...props} style={{ ...inputStyle, ...props.style }} />
+export const DSDate = ({ style, ...props }) => (
+  <input type="date" {...props} style={{ ...inputStyle, ...style }} />
 );
 
+// --------- <select> nativo con flechita ----------
 export const DSNativeSelect = ({ children, style, ...rest }) => (
   <div style={{ position: "relative", width: "100%" }}>
     <select
@@ -49,6 +52,7 @@ export const DSNativeSelect = ({ children, style, ...rest }) => (
   </div>
 );
 
+// --------- react-select con flechita personalizada ----------
 const DropdownArrow = (props) => (
   <components.DropdownIndicator {...props}>
     <svg
@@ -75,28 +79,41 @@ export const DSSelect = ({
     styles={{ ...RS_COMMON_STYLES, ...(styles || {}) }}
     components={{
       ...customComponents,
-      DropdownIndicator: DropdownArrow, // 👈 flechita personalizada
-      IndicatorSeparator: () => null,   // quita la barra fea
+      DropdownIndicator: DropdownArrow,
+      IndicatorSeparator: () => null,
     }}
     menuPortalTarget={menuPortalTarget ?? document.body}
     menuPosition={menuPosition}
   />
 );
 
+// --------- Botones ----------
+export const BtnPrimary = ({ children, style, ...p }) => (
+  <button {...p} style={{ ...primaryBtn, ...style }}>
+    {children}
+  </button>
+);
 
-// Botones
-export const BtnPrimary   = ({ children, style, ...p }) => (
-  <button {...p} style={{ ...primaryBtn, ...style }}>{children}</button>
-);
 export const BtnSecondary = ({ children, style, ...p }) => (
-  <button {...p} style={{ ...secondaryBtn, ...style }}>{children}</button>
+  <button {...p} style={{ ...secondaryBtn, ...style }}>
+    {children}
+  </button>
 );
-export const BtnEditDark  = ({ children, style, ...p }) => (
-  <button {...p} style={{ ...editBtnDark, ...style }}>{children}</button>
+
+export const BtnEditDark = ({ children, style, ...p }) => (
+  <button {...p} style={{ ...editBtnDark, ...style }}>
+    {children}
+  </button>
 );
-export const BtnDanger    = ({ children, style, ...p }) => (
-  <button {...p} style={{ ...dangerBtn, ...style }}>{children}</button>
+
+export const BtnDanger = ({ children, style, ...p }) => (
+  <button {...p} style={{ ...dangerBtn, ...style }}>
+    {children}
+  </button>
 );
+
 export const BtnTinyRound = ({ children, style, ...p }) => (
-  <button {...p} style={{ ...tinyRoundBtn, ...style }}>{children}</button>
+  <button {...p} style={{ ...tinyRoundBtn, ...style }}>
+    {children}
+  </button>
 );
